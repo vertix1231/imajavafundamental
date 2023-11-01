4c3ae27441cfef18ca3dde3bc30262c67837623e
Irsyad Muhammad Andre <irsyad.muhamad.andre@gmail.com>
2023-11-01 17:48:10 +0700

    praktikum 1 week 2

---
+++ src/main/java/com/juaracoding/imajavafundamental/testharian/testarray/TestArray.java
@@ -0,0 +1,35 @@
+package com.juaracoding.imajavafundamental.testharian.testarray;
+
+import java.util.Scanner;
+
+public class TestArray {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("(Input) Masukkan Kata: ");
+            String input = scanner.nextLine();
+            String inputlowe =input.toLowerCase();
+            String inputupp =input.toUpperCase();
+            int jumlahKarakter = hitungKarakter(input);
+            System.out.println("(Output) Hasil: " + jumlahKarakter);
+        }
+
+        public static int hitungKarakter(String input) {
+
+            int sumascii=0;
+            for (int i =0;i<input.length();i++){
+
+                int charascii = input.charAt(i);
+                if(!(charascii>=97&&charascii<=122)){
+                    System.out.println(charascii+" "+ input.charAt(i));
+                    sumascii +=charascii;
+                }
+            }
+
+
+            return sumascii;
+        }
+    }
+
+
+
